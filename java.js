5f2ae1416b3be73c471592575a731e636b5af889
marie-sb <msbecker00@gmail.com>
2023-04-29 14:58:26 +0200

    adding js

---
+++ java.js
@@ -0,0 +1,11 @@
+// A $( document ).ready() block.
+$( document ).ready(function() {
+
+    $(".hamburger-button").click(function(){
+
+        $(this).toggleClass('active');
+        $(".mobile-menu").fadeToggle();
+    });
+
+    
+});
